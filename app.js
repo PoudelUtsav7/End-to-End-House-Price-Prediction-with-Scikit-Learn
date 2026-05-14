48ff0bddc7d98066268b32d402066316c167f846
PoudelUtsav <poudelutsav057@gmail.com>
2026-05-14 14:56:45 +0545

    point to live API

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ async function predict(data) {
   errorPanel.style.display  = 'none'
 
   try {
-    const response = await fetch('http://127.0.0.1:8000/predict', {
+    const response = await fetch('https://house-price-predictor-9wtu.onrender.com/predict', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data)
